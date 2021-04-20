d7360776f1295f1c42dc07880dafbed7bae9d683
nanash1 <35433852+nanash1@users.noreply.github.com>
2021-04-20 10:58:55 +0200

    Update and rename f_jis_type_1.asm to f_write_str.asm

--- ASM Hacks/f_write_str.asm
+++ ASM Hacks/f_write_str.asm
@@ -2,9 +2,22 @@
 					.endian		little
 ;					.output		dbg
 ;
-; Modified drawing function to call f_write_char_hw
+; Writes a string to a line buffer. Modified to draw half wide characters by calling f_write_char_hw
 ;
+; Arguments
+; p_line_buff
+; x_pos
+; y_pos
+; p_str
+;
+; Variables
+p_str:					.reg	r12
+x_pos:					.reg	r14
+y_pos:					.reg	r9
+p_line_buff:				.reg	r4
+f_write_char_hw:			.reg	r11
 					.section main,code,locate=h'8c046c9c;
+f_write_str:
 					mov.l   r14, @-r15
 					mov.l   r12, @-r15
 					mov.l   r11, @-r15
@@ -12,14 +25,14 @@
 					mov.l   r9, @-r15
 					sts.l   pr, @-r15
 					mov.l   r4, @-r15
-					mov		#0, r10
+					mov	#0, r10
 					mov.l   r10, @-r15
 					mov.l   literal+4, r10
-					mov.l	literal+24,r11
-					mov     r7, r12
-					mov     r5, r14
+					mov.l	literal+24,f_write_char_hw
+					mov     r7, p_str
+					mov     r5, x_pos
 					bra     label2
-				 	mov     r6, r9
+				 	mov     r6, y_pos
 					nop
 literal:
 					.data.w	h'4000
@@ -31,24 +44,24 @@ literal:
 					.data.l	h'8c1be634
 					.data.l	h'8c0b4000
 loop:
-					mov     r14, r6
-					mov.w   @r12+, r5
-					mov     r9, r7
-					jsr		@r11
+					mov     x_pos, r6
+					mov.w   @p_str+, r5
+					mov     y_pos, r7
+					jsr	@f_write_char_hw
 					mov.l   @(4,r15), r4
-					add     #3, r14
+					add     #3, x_pos
 					mov     #h'1E, r2
-					cmp/ge  r2, r14
+					cmp/ge  r2, x_pos
 					bf    	label2
-					add     #3, r9
-					mov     #0, r14
+					add     #3, y_pos
+					mov     #0, x_pos
 label2:
-					mov.w   @r12, r3
+					mov.w   @p_str, r3
 					extu.w  r3, r3
 					cmp/eq  r10, r3
 					bf      loop
 					mov     #0, r0
-					add		#8, r15
+					add	#8, r15
 					lds.l   @r15+, pr
 					mov.l   @r15+, r9
 					mov.l   @r15+, r10
